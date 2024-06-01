f71af230d6e29050f8185c9e61d484405af2deab
aftekardic <aftekardic@outlook.com>
2024-06-01 12:13:07 +0300

    circular progress loading was added to auth provider for fix to navigation problem

--- frontend/src/services/AuthProvider.js
+++ frontend/src/services/AuthProvider.js
@@ -1,18 +1,63 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
     setIsAuthenticated(!!token);
-  }, []);
+
+    const timer = setTimeout(() => {
+      if (isAuthenticated === null) {
+        setError(true);
+        navigate("/");
+      }
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer); // Clean up the timer on component unmount
+  }, [isAuthenticated, navigate]);
+
+  if (loading) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        flexDirection="column"
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        flexDirection="column"
+      >
+        <Typography>Unable to authenticate. Redirecting...</Typography>
+      </Box>
+    );
+  }
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
-      {children}
+      {isAuthenticated ? children : <Navigate to={"/sign-in"} />}
     </AuthContext.Provider>
   );
 };
